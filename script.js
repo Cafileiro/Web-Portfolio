a8aefe93d3459f08984530be4b2505f572749b68
Rafael Iralde Dominguez <riralde@gmail.com>
2026-06-20 13:56:21 +0200

    feat(script): add funtions with gspa and scrolltrigger

--- script.js
+++ script.js
@@ -5,3 +5,69 @@ menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x')
     navbar.classList.toggle('active');
 }
+
+const container = document.querySelector(".container");
+const sections = gsap.utils.toArray(".container section");
+const texts = gsap.utils.toArray(".anim");
+const mask = document.querySelector(".mask");
+
+let scrollTween = gsap.to(sections, {
+  xPercent: -100 * (sections.length - 1),
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".container",
+    pin: true,
+    scrub: 1,
+    end: "+=3000",
+    //snap: 1 / (sections.length - 1),
+  }
+});
+
+console.log(1 / (sections.length - 1))
+
+//Progress bar animation
+
+gsap.to(mask, {
+  width: "100%",
+  scrollTrigger: {
+    trigger: ".wrapper",
+    start: "top left",
+    scrub: 1
+  }
+});
+
+// whizz around the sections
+sections.forEach((section) => {
+  // grab the scoped text
+  let text = section.querySelectorAll(".anim");
+  
+  // bump out if there's no items to animate
+  if(text.length === 0)  return 
+  
+  // do a little stagger
+  gsap.from(text, {
+    y: -130,
+    opacity: 0,
+    duration: 2,
+    ease: "elastic",
+    stagger: 0.1,
+    scrollTrigger: {
+      trigger: section,
+      containerAnimation: scrollTween,
+      start: "left center",
+    }
+  });
+});
+
+ScrollTrigger.create({
+  trigger: ".wrapper",
+  start: "top top",
+  end: "+=3000",
+  onToggle: self => {
+    gsap.to("header", {
+      y: self.isActive ? -100 : 0,
+      opacity: self.isActive ? 0 : 1,
+      duration: 0.3
+    });
+  }
+});
